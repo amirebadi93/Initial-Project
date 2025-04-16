658c18ef69dfe2f9dfe7706108a2d6b5ecbafbe0
AmirHossein Ebadi F. Azar <Amir.ebadi93@hotmail.com>
2025-04-16 20:43:12 +0330

    todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState();
+  const [list, setList] = useState(["Test", "test1", "test2"]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <div className="form">
+        <input
+          type="text"
+          value={value}
+          onChange={e => {
+            const tempValue = e.target.value;
+            setValue(tempValue);
+          }}
+        />
+
+        <button
+          onClick={() => {
+            const updatedList = [...list];
+            updatedList.push(value);
+            setList(updatedList);
+            setValue("");
+          }}
+        >
+          add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <span>
+        {value}
+      </span>
+      <div className="todo-list">
+        <ul>
+          {list.map(item =>
+            <li>
+              {item}
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
